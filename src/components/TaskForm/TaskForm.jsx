5c7a5bf732efdbc1a60f1ab430c19bc1eb5e116b
Fede <defe014@gmail.com>
2022-06-13 18:33:36 -0300

    listo los estilos de task

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -2,10 +2,24 @@ import './TaskForm.style.css'
 
 const TaskForm = () => {
   return (
-    <section className="task-form">
-      <h2>Crear Tarea</h2>
-      <p>Crea tus tareas</p>
-    </section>
+        <section className="task-form">
+            <h2>Crear Tarea</h2>
+            <p>Crea tus tareas</p>
+            <form>
+                <div>
+                    <div>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <select type="text" />
+                    </div>
+                    <div>
+                        <select type="text" />
+                    </div>
+                </div>
+                <div></div>
+            </form>
+        </section>
   )
 }
 
