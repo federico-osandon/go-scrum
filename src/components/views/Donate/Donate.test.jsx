06e114961ff58abbec4c6d495e4c00590d4f33aa
Fede <defe014@gmail.com>
2022-07-14 00:25:27 -0300

    listo lo de test

---
+++ src/components/views/Donate/Donate.test.jsx
@@ -0,0 +1,21 @@
+// import { describe, expect, it } from 'vitest';
+// import Donate from './Donate';
+
+// describe("Donate", () => {
+//   it("renderiza h1", () => {
+//     const result = <Donate />
+//     expect(result).toBe(2);
+//   })
+// })
+
+
+
+
+// import { render, screen } from  '@testing-library/react'
+// import Donate from './Donate'
+
+// it('renderiza un h1', ()=>{
+//     render(<Donate />)
+    
+//     expect(screen.getByRole('heading')).toBeInTheDocument()
+// })
