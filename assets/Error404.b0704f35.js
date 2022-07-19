60cd3258a7ec6eba6cdebda6b5cc00d0319a9c60
Fede <defe014@gmail.com>
2022-07-18 23:43:55 -0300

    Updates

--- assets/Error404.b0704f35.js
+++
@@ -1,1 +0,0 @@
-import{j as a,a as r}from"./index.7cac76fb.js";const e=()=>a("div",{className:"container",children:[r("h1",{children:"Error 404"}),r("br",{}),r("h3",{children:"P\xE1gina no encontrada"})]});export{e as default};
